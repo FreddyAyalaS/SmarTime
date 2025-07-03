c255cc5e39e863544e8740a20d3457a46e6e8d5a
DiegoGC1822 <fenixly1029@gmail.com>
2025-07-03 12:19:08 -0500

    cambio url

--- Backend/SmarTime/settings.py
+++ Backend/SmarTime/settings.py
@@ -192,7 +192,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React dev server
-    "https://smartime-production.up.railway.app",  # URL de producción
+    "https://smartime-production-35f3.up.railway.app",  # URL de producción
 ]
 
 
