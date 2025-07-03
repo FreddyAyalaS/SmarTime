c67af6ee5d1a4a0988a70f99958cbbf3f7ecb2f0
DiegoGC1822 <fenixly1029@gmail.com>
2025-07-03 02:41:45 -0500

    url cambio contraseña

--- Backend/Apps/Autenticacion/views.py
+++ Backend/Apps/Autenticacion/views.py
@@ -5,7 +5,11 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegistroUsuarioSerializer, PasswordResetRequestSerializer, SetNewPasswordSerializer
+from .serializers import (
+    RegistroUsuarioSerializer,
+    PasswordResetRequestSerializer,
+    SetNewPasswordSerializer,
+)
 from .models import UsuarioPersonalizado
 from rest_framework.permissions import AllowAny
 
@@ -14,6 +18,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.mail import send_mail
 from django.conf import settings
+import os
 
 
 class RegistroUsuarioAPIView(APIView):
@@ -21,24 +26,34 @@ class RegistroUsuarioAPIView(APIView):
         serializer = RegistroUsuarioSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"mensaje": "Usuario creado"}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"mensaje": "Usuario creado"}, status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LoginUsuarioAPIView(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         usuario = authenticate(username=username, password=password)
         if usuario:
             refresh = RefreshToken.for_user(usuario)
-            return Response({
-                "mensaje": "Login exitoso",
-                "access": str(refresh.access_token),
-                "refresh": str(refresh),
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "mensaje": "Login exitoso",
+                    "access": str(refresh.access_token),
+                    "refresh": str(refresh),
+                },
+                status=status.HTTP_200_OK,
+            )
         else:
-            return Response({"error": "Credenciales incorrectas"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Credenciales incorrectas"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
 
 class LogoutUsuarioAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -46,30 +61,42 @@ class LogoutUsuarioAPIView(APIView):
     def post(self, request):
         try:
             # Logout con simplejwt y blacklist
-            refresh_token = request.data.get('refresh')
+            refresh_token = request.data.get("refresh")
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response({"mensaje": "Logout exitoso"}, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({"error": f"No se pudo cerrar sesión: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": f"No se pudo cerrar sesión: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class PasswordResetRequestAPIView(APIView):
-    permission_classes = [AllowAny] #Cualquiera tiene acceso, ya que al no tener contraseña tiene que poder usarla, así no esté autenticado.
+    permission_classes = [
+        AllowAny
+    ]  # Cualquiera tiene acceso, ya que al no tener contraseña tiene que poder usarla, así no esté autenticado.
 
     def post(self, request):
         serializer = PasswordResetRequestSerializer(data=request.data)
         if serializer.is_valid():
-            email = serializer.validated_data['email']
+            email = serializer.validated_data["email"]
             try:
                 user = UsuarioPersonalizado.objects.get(email=email)
             except UsuarioPersonalizado.DoesNotExist:
-                return Response({"error": "No existe usuario con ese correo"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "No existe usuario con ese correo"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             token_generator = PasswordResetTokenGenerator()
             token = token_generator.make_token(user)
             uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-            reset_link = f"http://localhost:3000/reset-password/{uidb64}/{token}/"
+            if os.getenv("ENV") == "production":
+                url = "https://smartime-production.up.railway.app/"
+            else:
+                url = "http://localhost:3000/"
+            reset_link = f"{url}/{uidb64}/{token}/"
 
             # Envío de correo real
             subject = "Restablece tu contraseña"
@@ -77,13 +104,18 @@ class PasswordResetRequestAPIView(APIView):
             from_email = settings.DEFAULT_FROM_EMAIL
             recipient_list = [email]
 
-            send_mail(subject, message, from_email, recipient_list, fail_silently=False) #Envía el correo usando la configuración SMTP definida en settings.py
+            send_mail(
+                subject, message, from_email, recipient_list, fail_silently=False
+            )  # Envía el correo usando la configuración SMTP definida en settings.py
 
-            return Response({"mensaje": "Se ha enviado un enlace para restablecer la contraseña al correo."}, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "mensaje": "Se ha enviado un enlace para restablecer la contraseña al correo."
+                },
+                status=status.HTTP_200_OK,
+            )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 
 
 class SetNewPasswordAPIView(APIView):
@@ -92,51 +124,47 @@ class SetNewPasswordAPIView(APIView):
     def post(self, request):
         serializer = SetNewPasswordSerializer(data=request.data)
         if serializer.is_valid():
-            password = serializer.validated_data['password']
-            token = serializer.validated_data['token']
-            uidb64 = serializer.validated_data['uidb64']
+            password = serializer.validated_data["password"]
+            token = serializer.validated_data["token"]
+            uidb64 = serializer.validated_data["uidb64"]
 
             try:
                 uid = force_str(urlsafe_base64_decode(uidb64))
                 user = UsuarioPersonalizado.objects.get(pk=uid)
-            except (TypeError, ValueError, OverflowError, UsuarioPersonalizado.DoesNotExist):
-                return Response({"error": "UID inválido"}, status=status.HTTP_400_BAD_REQUEST)
+            except (
+                TypeError,
+                ValueError,
+                OverflowError,
+                UsuarioPersonalizado.DoesNotExist,
+            ):
+                return Response(
+                    {"error": "UID inválido"}, status=status.HTTP_400_BAD_REQUEST
+                )
 
             token_generator = PasswordResetTokenGenerator()
             if not token_generator.check_token(user, token):
-                return Response({"error": "Token inválido o expirado"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Token inválido o expirado"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             if user.check_password(password):
-                return Response({"error": "La nueva contraseña no puede ser igual a la anterior."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "La nueva contraseña no puede ser igual a la anterior."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             user.set_password(password)
             user.save()
-            return Response({"mensaje": "Contraseña actualizada con éxito"}, status=status.HTTP_200_OK)
+            return Response(
+                {"mensaje": "Contraseña actualizada con éxito"},
+                status=status.HTTP_200_OK,
+            )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''from rest_framework.views import APIView
+"""from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
@@ -161,27 +189,10 @@ class LoginUsuarioAPIView(APIView):
             return Response({"mensaje": "Login exitoso"}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Credenciales incorrectas"}, status=status.HTTP_401_UNAUTHORIZED)
-'''
+"""
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''from rest_framework.views import APIView
+"""from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
@@ -221,4 +232,4 @@ class LoginUsuario(APIView):
             return Response({"mensaje": "Login exitoso"}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Credenciales incorrectas"}, status=status.HTTP_401_UNAUTHORIZED)
-'''
+"""
